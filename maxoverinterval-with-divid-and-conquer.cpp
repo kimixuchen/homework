03756f1f97b562f334319f481b93bd8ffa22723d
xuchen <kimixuchen@163.com>
2015-09-29 11:08:24 +0800

    correct some mistakes

--- maxoverinterval-with-divid-and-conquer.cpp
+++ maxoverinterval-with-divid-and-conquer.cpp
@@ -1,5 +1,5 @@
 /**
-2015-9-25
+2015-9-29
 xuchen
 **/
 #include "stdio.h"
@@ -11,46 +11,53 @@ using namespace std;
 int maxoverlap;
 vector< pair<int, int> > intervals;
 
-void solve(int l, int r)
+bool cmp(pair<int, int> a, pair<int, int> b)
+{
+	return a.first<b.first;
+}
+
+int solve(int l, int r)
 {
 	if(l>=r)
-		return ;
-	pair<int, int> x = intervals[l];
-	int pos = l;
+		return 0;
+	int mid = (l+r)/2;
+	int lMax, rMax;
+	int mMax = 0;
 	int cover;
-	pair<int, int> tmp;
-	int right;
-	for(int i=l+1; i<=r; i++)
+	int end;
+	int lEndMax = 0;
+	pair<int, int> x = intervals[mid];
+	
+	for(int i=l; i<=mid-1; i++)
+		lEndMax = max(lEndMax, intervals[i].second);
+	
+	if(lEndMax>x.first && lEndMax<=x.second)
 	{
-		if(intervals[i].first<x.first)
-		{
-			if(intervals[i].second>=x.first)
-			{
-				right = min(intervals[i].second, x.second);
-				cover = right - x.first;
-				maxoverlap = max(maxoverlap, cover);
-			}
-			pos++;
-			tmp = intervals[pos];
-			intervals[pos] = intervals[i];
-			intervals[i] = tmp;
-		}
-		else
-		{
-			if(x.second>=intervals[i].first)
-			{
-				right = min(x.second, intervals[i].second);
-				cover = right - intervals[i].first;
-				maxoverlap = max(maxoverlap, cover);
-			}
-		}
+		mMax = lEndMax-x.first;
+		lEndMax = x.second;
 	}
-	tmp = intervals[pos];
-	intervals[pos] = x;
-	intervals[l] = tmp;
-
-	solve(l, pos-1);
-	solve(pos+1, r);	
+	else if(lEndMax>x.second)
+	{
+		mMax = x.second-x.first;
+	}
+	else
+		lEndMax = x.second;
+	
+	for(int i=mid+1; i<=r; i++)
+	{
+		if(intervals[i].first>lEndMax)
+			break;
+		end = min(intervals[i].second, lEndMax);
+		cover = end-intervals[i].first;
+		mMax = max(mMax, cover);
+	}
+	
+	lMax = solve(l, mid-1);
+	rMax = solve(mid+1, r);
+	lMax = max(lMax, rMax);
+	
+	return max(mMax, lMax);
+	
 }
 
 int main()
@@ -63,9 +70,13 @@ int main()
 		else
 			intervals.push_back(make_pair(b, a));
 	}
+	sort(intervals.begin(), intervals.end(), cmp);
+	
 	size = intervals.size();
-	solve(0, size-1);
+	maxoverlap = solve(0, size-1);
 	printf("%d\n", maxoverlap);
 
 	return 0;
 }
+
+
